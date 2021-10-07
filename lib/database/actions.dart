76d06cbf831bea25862e746a3fdee7a809766d05
Jjmesia <jjmesia@mcm.edu.ph>
2021-10-08 05:35:44 +0800

    Reworked the homescreen design and card

--- lib/database/actions.dart
+++ lib/database/actions.dart
@@ -5,16 +5,17 @@ import '../under_development.dart';
 class Actions {
   String action;
   String icon;
+  String color;
   Widget Function() navigateTo;
 
-  Actions(this.action, this.icon, this.navigateTo);
+  Actions(this.action, this.icon, this.color, this.navigateTo);
 }
 
 var actions = [
-  Actions("Money\nTransfer", "assets/money_transfer_blue.svg",
+  Actions("Money\nTransfer", "0xFF00142D", "assets/money_transfer_blue.svg",
       () => HomePageScreen()),
-  Actions("Bank\nWithdraw", "assets/bank_withdraw_blue.svg",
+  Actions("Bank\nWithdraw", "0xFF00142D", "assets/bank_withdraw_blue.svg",
       () => UnderDevelopmentScreen()),
-  Actions("Insight\nTracking", "assets/insight_tracking_blue.svg",
+  Actions("Insight\nTracking", "0xFF00142D", "assets/insight_tracking_blue.svg",
       () => UnderDevelopmentScreen()),
 ];
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,168 +9,199 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Color(0xFF252525),
-        body: Container(
-            margin: EdgeInsets.only(top: 8),
-            child: Padding(
-              padding: const EdgeInsets.all(18),
-              child: ListView(
-                physics: ClampingScrollPhysics(),
-                children: [
-                  // MENU BAR
-                  Container(
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.menu_rounded,
-                          color: Colors.grey[400],
-                          size: 40,
-                        )
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  // HEADER
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Welcome back,",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.w900)),
-                        Text("\t Admin",
-                            style: TextStyle(
-                                color: Colors.blue[900],
-                                fontSize: 30,
-                                fontWeight: FontWeight.w900))
-                      ]),
-                  SizedBox(height: 20),
-                  Container(
-                      height: 199,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: accounts.length,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              margin: EdgeInsets.only(right: 10),
-                              height: 199,
-                              width: 344,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  gradient: LinearGradient(
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight,
-                                      colors: <Color>[
-                                        Color(0xFF715FE0),
-                                        Color(0xFF792D3D)
-                                      ]),
-                                  boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.grey.withOpacity(0.1),
-                                        spreadRadius: 3,
-                                        blurRadius: 1,
-                                        offset: Offset(0, 5))
-                                  ]),
-                              child: Stack(
-                                children: [
-                                  Positioned(
-                                    left: 30,
-                                    top: 25,
-                                    child: Text("ACCOUNT NUMBER",
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 10)),
-                                  ),
-                                  Positioned(
-                                    left: 30,
-                                    top: 40,
-                                    child: Text(accounts[index].accNumber,
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: 20)),
-                                  ),
-                                  Positioned(
-                                    right: 30,
-                                    bottom: 45,
-                                    child: Text(
-                                        "₱" +
-                                            accounts[index].balance.toString(),
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: 25)),
-                                  ),
-                                  Positioned(
-                                    right: 30,
-                                    bottom: 30,
-                                    child: Text("AVAILABLE BALANCE",
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 10)),
-                                  )
-                                ],
-                              ),
-                            );
-                          })),
-                  SizedBox(height: 30),
-                  Text("Actions:",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w800)),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
+    return Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Color(0xFF252525),
+              Color(0xFF414141),
+              Color(0xFF212121)
+            ])),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(
+                margin: EdgeInsets.only(top: 8),
+                child: Padding(
+                    padding: const EdgeInsets.all(18),
+                    child:
+                        ListView(physics: ClampingScrollPhysics(), children: [
+                      Padding(padding: const EdgeInsets.symmetric(vertical: 5)),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Welcome back,",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w900)),
+                            Text("\t Admin",
+                                style: TextStyle(
+                                    color: Colors.blue[800],
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w900))
+                          ]),
+                      SizedBox(height: 20),
                       Container(
-                          height: 175,
+                          height: 199,
                           child: ListView.builder(
-                            itemCount: actions.length,
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, index) {
-                              var current;
-                              return GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              actions[index].navigateTo()));
+                              scrollDirection: Axis.horizontal,
+                              itemCount: accounts.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  margin: EdgeInsets.only(right: 10),
+                                  height: 199,
+                                  width: 344,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      gradient: LinearGradient(
+                                          begin: Alignment.topLeft,
+                                          end: Alignment.bottomRight,
+                                          colors: <Color>[
+                                            Color(0xFF00142D),
+                                            Color(0xFF003478),
+                                            Color(0xFFF25A14),
+                                          ]),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color:
+                                                Colors.white.withOpacity(0.6),
+                                            spreadRadius: 1,
+                                            blurRadius: 1,
+                                            offset: Offset(3, 2))
+                                      ]),
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                        left: 28,
+                                        top: 35,
+                                        child: Text("ACCOUNT NUMBER",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w300)),
+                                      ),
+                                      Positioned(
+                                        left: 30,
+                                        bottom: 21,
+                                        child: Text(accounts[index].user,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 16)),
+                                      ),
+                                      Positioned(
+                                        left: 30,
+                                        bottom: 45,
+                                        child: Text("CARD HOLDERNAME",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w400)),
+                                      ),
+                                      Positioned(
+                                        left: 32,
+                                        top: 60,
+                                        child: Text(accounts[index].accNumber,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 20)),
+                                      ),
+                                      Positioned(
+                                        right: 18,
+                                        child: Image.asset(
+                                          accounts[index].cardType,
+                                          width: 80,
+                                          height: 80,
+                                        ),
+                                      ),
+                                      Positioned(
+                                        right: 28,
+                                        bottom: 18,
+                                        child: Text(
+                                            "₱" +
+                                                accounts[index]
+                                                    .balance
+                                                    .toString(),
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize: 25)),
+                                      ),
+                                      Positioned(
+                                        right: 36,
+                                        bottom: 48,
+                                        child: Text("WALLET BALANCE",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.w300)),
+                                      )
+                                    ],
+                                  ),
+                                );
+                              })),
+                      SizedBox(height: 25),
+                      Text("Actions:",
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w900)),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              height: 165,
+                              width: 270,
+                              child: ListView.builder(
+                                itemCount: actions.length,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  var current;
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  actions[index].navigateTo()));
+                                    },
+                                    child: ActionCard(
+                                        action: actions[index].action,
+                                        icon: actions[index].icon,
+                                        isSelected: current == index,
+                                        context: this),
+                                  );
                                 },
-                                child: ActionCard(
-                                    action: actions[index].action,
-                                    icon: actions[index].icon,
-                                    isSelected: current == index,
-                                    context: this),
-                              );
-                            },
-                          )),
-                    ],
-                  ),
-                  Container(
-                    width: double.infinity,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      UnderDevelopmentScreen()));
-                        },
-                        child: Text("View Transaction History",
-                            style:
-                                TextStyle(color: Colors.white, fontSize: 16)),
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18)))),
+                              )),
+                        ],
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )));
+                      Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        UnderDevelopmentScreen()));
+                          },
+                          child: Text("View Transaction History",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w900)),
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(20)))),
+                        ),
+                      ),
+                    ])))));
   }
 }
